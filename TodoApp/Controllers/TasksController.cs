11bc346ead9c1fe462ee44265c7114fb997f2813
clrhost-dll <laykmin15@gmail.com>
2026-05-27 01:31:23 +0300

    feat: add TaskController

---
+++ TodoApp/Controllers/TasksController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using TodoApp.BLL.DTOs.Tasks;
+using TodoApp.BLL.Interfaces;
+
+namespace TodoApp.Controllers
+{
+    [ApiController]
+    [Route("api/tasks")]
+    [Authorize]
+    public class TasksController : ControllerBase
+    {
+        private readonly ITaskService _taskService;
+
+        public TasksController(ITaskService taskService)
+        {
+            _taskService = taskService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll(
+            [FromQuery] TaskQueryDto query)
+        {
+            Guid userId = GetUserId();
+
+            var result = await _taskService.GetAllAsync(
+                userId,
+                query);
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var result =
+                await _taskService.GetByIdAsync(id);
+
+            if (result is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(
+            CreateTaskDto dto)
+        {
+            Guid userId = GetUserId();
+
+            await _taskService.CreateAsync(
+                userId,
+                dto);
+
+            return Ok();
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> Update(
+            Guid id,
+            UpdateTaskDto dto)
+        {
+            await _taskService.UpdateAsync(id, dto);
+
+            return Ok();
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await _taskService.DeleteAsync(id);
+
+            return Ok();
+        }
+
+        private Guid GetUserId()
+        {
+            return Guid.Parse(
+                User.FindFirstValue(
+                    ClaimTypes.NameIdentifier)!);
+        }
+    }
+}
